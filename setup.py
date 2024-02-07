6e8a4fc77dc350684a387310736a2fa8b3976d46
lindong8680 <116825246+lindong8680@users.noreply.github.com>
2024-02-07 17:20:50 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myproxy',
+    version='0.1',
+    packages=find_packages(),
+    description='get A max health proxy',
+    author='dong lin',
+    author_email='15241848680@163.com',
+    url='https://github.com/yourusername/my_package',
+)
